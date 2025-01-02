137caaaade39fce197d6507de10c4776a56ae91d
Hirwa9 <hirwawilly9@gmail.com>
2025-01-02 17:12:37 +0200

    Created featured properties component

---
+++ src/components/home/featured/FeaturedProps.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Heading from '../../common/Heading';
+import PropertyCard from '../../common/PropertyCard';
+
+const FeaturedProps = () => {
+    return (
+        <>
+            <section>
+                <div className="container">
+                    <Heading
+                        title="Our Featured Listing"
+                        subtitle="Discover our top-tier featured properties. Stay ahead with the most desirable listings and receive expert support to make well-informed decisions, all backed by comprehensive details. Keep an eye on these prime opportunities before they’re gone"
+                        hType="h2"
+                    />
+                    <PropertyCard filterOption="featured" />
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default FeaturedProps;
