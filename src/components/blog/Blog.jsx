1bfa6251cef9543f32b33625991b26036f8bd4d5
Hirwa9 <hirwawilly9@gmail.com>
2025-01-02 17:40:22 +0200

    Saved changes to a renamed components 'PageInfo.jsx'

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Back from '../common/header/Back';
+import PageInfo from '../common/header/PageInfo';
 import img from '../images/about.jpeg';
 import PropertyCard from '../common/PropertyCard';
 
@@ -7,7 +7,7 @@ const Blog = () => {
     return (
         <>
             <section className='mb-4 blog-out'>
-                <Back name="Blog" title="Blog grid - Our Blogs" cover={img} />
+                <PageInfo name="Blog" title="Blog grid - Our Blogs" cover={img} />
                 <div className="container recent">
                     <PropertyCard />
                 </div>
