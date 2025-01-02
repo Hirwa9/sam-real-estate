0291f433a3a2094debbbc96fc064d40bac83ec20
Hirwa9 <hirwawilly9@gmail.com>
2025-01-02 17:53:52 +0200

    Saved changes to renamed components

--- src/components/pricing/Pricing.jsx
+++ src/components/pricing/Pricing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Back from '../common/header/Back';
+import PageInfo from '../common/header/PageInfo';
 import img from '../images/pricing.jpg';
 import PriceCard from '../home/price/PriceCard';
 
@@ -7,7 +7,7 @@ const Pricing = () => {
     return (
         <>
             <section className='mb-4 service'>
-            <Back name="30 days money back guarantee" title="No Extra Fees. FriendIy Support" cover={img}/>
+            <PageInfo name="30 days money back guarantee" title="No Extra Fees. FriendIy Support" cover={img}/>
                 <div className="price container">
                     <PriceCard />
                 </div>
