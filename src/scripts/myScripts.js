e1da81b6a8656ca19ed233b05c30ab4b72fbbb76
hirwa9 <hirwawilly9@gmail.com>
2025-03-03 21:55:05 +0200

    Refine big number format helper function

--- src/scripts/myScripts.js
+++ src/scripts/myScripts.js
@@ -77,13 +77,13 @@ export const formatBigCountNumbers = (number) => {
     let formattedNumber;
     switch (true) {
         case (number >= 1000000000):
-            formattedNumber = (number / 1000000000).toFixed(1) + " B";
+            formattedNumber = (number / 1000000000).toFixed(1) + "B +";
             break;
         case (number >= 1000000):
-            formattedNumber = (number / 1000000).toFixed(1) + " M";
+            formattedNumber = (number / 1000000).toFixed(1) + "M +";
             break;
         case (number >= 1000):
-            formattedNumber = (number / 1000).toFixed(1) + " K";
+            formattedNumber = (number / 1000).toFixed(1) + "K +";
             break;
         default:
             formattedNumber = number.toString();
