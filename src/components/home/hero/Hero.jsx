8a9f02132ed8bd800af3d909ec0e13249e75afef
hirwa9 <hirwawilly9@gmail.com>
2025-03-11 17:42:43 +0200

    Search properties using a combined object query instead if individual query paramaters

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -1,17 +1,32 @@
 import React, { useRef, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "./hero.css"
+import "./hero.css";
 import Heading from "../../common/Heading";
 import { aboutProperties } from "../../data/Data";
-import { Bed, Building, CaretDown, CaretRight, MagnifyingGlass, MapPinArea, MoneyWavy } from "@phosphor-icons/react";
+import { Bed, Building, CaretDoubleRight, CaretDown, CaretRight, MapPinArea, MoneyWavy } from "@phosphor-icons/react";
+import useCustomDialogs from "../../hooks/useCustomDialogs";
+import MyToast from "../../common/Toast";
 
 const Hero = () => {
+    // Custom hooks
+    const {
+        // Toast
+        showToast,
+        toastMessage,
+        toastType,
+        toast,
+        resetToast,
+    } = useCustomDialogs();
+
     const searcherRef = useRef();
     const navigate = useNavigate();
 
     // All input values
     const [formData, setFormData] = useState({
         propertySearcher: '',
+        propertyType: '',
+        priceRange: '',
+        bedrooms: ''
     });
 
     // General handleChange function
@@ -23,43 +38,26 @@ const Hero = () => {
         });
     };
 
-    // Search by input
+    // Handle search
     const handleSearch = () => {
-        const searchString = formData.propertySearcher;
-        if (searchString && searchString !== '' && searchString.length > 3) {
-            const query = "search_query=" + searchString;
-            navigate(`/properties/${query}`);
+        const { propertySearcher, propertyType, priceRange, bedrooms } = formData;
+        // Check for at least one valid search criteria
+        if (!propertySearcher && !propertyType && !priceRange && !bedrooms) {
+            return toast({ message: 'Please fill in at least one search criteria', type: 'gray-700' });
         }
-    };
-
-    // Search by type selection
-    const handleTypeSelection = (e) => {
-        const selectedType = e.target.value;
-        if (selectedType) {
-            navigate(`/properties/${selectedType}`); // Navigate to the corresponding type
-        }
-    };
-
-    // Search by price range selection
-    const handlePriceRangeSelection = (e) => {
-        const selectedRange = e.target.value;
-        const query = "priceRange_" + selectedRange;
-        if (selectedRange) {
-            navigate(`/properties/${query}`); // Navigate to the corresponding type
-        }
-    };
-
-    // Search by bedroom selection
-    const handleBedroomSelection = (e) => {
-        const selectedNumber = e.target.value;
-        if (selectedNumber) {
-            const query = "bedrooms_" + selectedNumber;
-            navigate(`/properties/${query}`); // Navigate to the corresponding type
-        }
+        // Navigate to properties page with search query
+        const query = new URLSearchParams({
+            location: propertySearcher,
+            type: propertyType,
+            price: priceRange,
+            bedrooms: bedrooms
+        }).toString();
+        navigate(`/properties/search?${query}`);
     };
 
     return (
         <>
+            <MyToast show={showToast} message={toastMessage} type={toastType} selfClose onClose={resetToast} />
             <section className="d-flex flex-column hero">
                 <div className="mt-auto container">
                     <Heading
@@ -71,56 +69,63 @@ const Hero = () => {
                         subtitleClassName="w-fit mx-auto px-4 px-md-5 py-3 bg-bodi"
                     />
                     <div className="mb-3 px-2 pt-3 bg-bodi blur-bg-2px text-gray-700 property-filter">
-                        <div className="d-md-flex flex-wrap align-items-center justify-content-between ">
-                            <div className="d-grid p-2 col-md-6 col-lg-4 box">
-                                <span className="flex-align-center mb-2 fw-bold small"><MapPinArea weight='bold' className='me-2 opacity-50' />Location</span>
-                                <div className="d-flex">
+
+                        <div className="d-flex flex-column flex-xl-row">
+                            <div className="col-xl-10 d-md-flex flex-wrap align-items-center justify-content-between">
+                                <div className="d-grid p-2 col-md-6 box">
+                                    <span className="flex-align-center mb-2 fw-bold small"><MapPinArea weight='bold' className='me-2 opacity-50' />Location</span>
                                     <input ref={searcherRef} type="text" name="propertySearcher" id="" placeholder="City / Street" className="p-2 border border-0 w-100" value={formData.propertySearcher} onChange={handleChange} onKeyUp={(e) => { (e.key === "Enter") && handleSearch() }} />
-                                    <button className="btn mx-1 rounded-pill" onClick={() => handleSearch()}><MagnifyingGlass weight='bold' size={20} /></button>
+                                </div>
+                                <div className="d-grid p-2 col-md-6 box">
+                                    <span className="flex-align-center mb-2 fw-bold small"><Building weight='bold' className='me-2 opacity-50' /> Property type</span>
+                                    <select id="filterParametersType" className="p-2 border border-0 w-100 ptr"
+                                        name="propertyType"
+                                        value={formData.propertyType}
+                                        onChange={handleChange}>
+                                        <option value="" disabled>Property type</option>
+                                        {aboutProperties.allTypes
+                                            .sort((a, b) =>
+                                                a.localeCompare(b)
+                                            )
+                                            .map((val, index) => (
+                                                <option key={index} value={val} className='dropdown-item p-2 px-3 small'>
+                                                    {val}
+                                                </option>
+                                            ))}
+                                    </select>
+                                </div>
+                                <div className="d-grid p-2 col-md-6 box">
+                                    <span className="flex-align-center mb-2 fw-bold small"><MoneyWavy weight='bold' className='me-2 opacity-50' />Price range</span>
+                                    <select id="filterParametersPriceRange" className="p-2 border border-0 w-100 ptr"
+                                        name="priceRange"
+                                        value={formData.priceRange}
+                                        onChange={handleChange}>
+                                        <option value="" disabled>Price range (RWF)</option>
+                                        {aboutProperties.priceRanges
+                                            .map((val, index) => (
+                                                <option key={index} value={val.min.replaceAll(',', '') + 'and' + val.max.replaceAll(',', '')}
+                                                    className='dropdown-item p-2 px-3 small'>
+                                                    {val.min} - {val.max}
+                                                </option>
+                                            ))}
+                                    </select>
+                                </div>
+                                <div className="d-grid p-2 col-md-6 box">
+                                    <span className="flex-align-center mb-2 fw-bold small"><Bed weight='bold' className='me-2 opacity-50' />Bedrooms</span>
+                                    <select id="filterParametersBedrooms" className="p-2 border border-0 w-100 ptr"
+                                        name="bedrooms"
+                                        value={formData.bedrooms}
+                                        onChange={handleChange}>
+                                        <option value="" disabled>Bedrooms</option>
+                                        {Array.from({ length: 6 }).map((_, index) => (
+                                            <option key={index} value={index + 1}>{index + 1}</option>
+                                        ))}
+                                    </select>
                                 </div>
                             </div>
-                            <div className="d-grid p-2 col-md-6 col-lg-2 box">
-                                <span className="flex-align-center mb-2 fw-bold small"><Building weight='bold' className='me-2 opacity-50' /> Property type</span>
-                                <select id="filterParametersType" className="p-2 border border-0 w-100 ptr"
-                                    defaultValue=""
-                                    onChange={handleTypeSelection}>
-                                    <option value="" disabled>Property type</option>
-                                    {aboutProperties.allTypes
-                                        .sort((a, b) =>
-                                            a.localeCompare(b)
-                                        )
-                                        .map((val, index) => (
-                                            <option key={index} value={val} className='dropdown-item p-2 px-3 small'>
-                                                {val}
-                                            </option>
-                                        ))}
-                                </select>
-                            </div>
-                            <div className="d-grid p-2 col-md-6 col-lg-4 box">
-                                <span className="flex-align-center mb-2 fw-bold small"><MoneyWavy weight='bold' className='me-2 opacity-50' />Price range</span>
-                                <select id="filterParametersPriceRange" className="p-2 border border-0 w-100 ptr"
-                                    defaultValue=""
-                                    onChange={handlePriceRangeSelection}>
-                                    <option value="" disabled>Price range (RWF)</option>
-                                    {aboutProperties.priceRanges
-                                        .map((val, index) => (
-                                            <option key={index} value={val.min.replaceAll(',', '') + 'and' + val.max.replaceAll(',', '')}
-                                                className='dropdown-item p-2 px-3 small'>
-                                                {val.min} - {val.max}
-                                            </option>
-                                        ))}
-                                </select>
-                            </div>
-                            <div className="d-grid p-2 col-md-6 col-lg-2 box">
-                                <span className="flex-align-center mb-2 fw-bold small"><Bed weight='bold' className='me-2 opacity-50' />Bedrooms</span>
-                                <select id="filterParametersBedrooms" className="p-2 border border-0 w-100 ptr"
-                                    defaultValue=""
-                                    onChange={handleBedroomSelection}>
-                                    <option value="" disabled>Bedrooms</option>
-                                    {Array.from({ length: 6 }).map((_, index) => (
-                                        <option key={index} value={index + 1}>{index + 1}</option>
-                                    ))}
-                                </select>
+                            <div className="w-fit col-xl-2 mx-auto my-3 d-flex align-items-center gap-2 ps-3 fw-bold confirm-search ptr clickDown shadow-sm" onClick={() => handleSearch()}>
+                                <span>Search</span>
+                                <span className="py-2 rounded-pill text-inherit"><CaretDoubleRight weight='bold' size={18} /></span>
                             </div>
                         </div>
                         <nav className="navbar navbar-expand-md px-2 pb-4">
