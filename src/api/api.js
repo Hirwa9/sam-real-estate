8ef40dcd7c3a07db1d4b1d34edfab0645595d2f2
hirwa9 <hirwawilly9@gmail.com>
2025-03-13 09:11:34 +0200

    Send cookies in all API requests

--- src/api/api.js
+++ src/api/api.js
@@ -3,4 +3,5 @@ export const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export const Axios = axios.create({
     baseURL: BASE_URL,
+    withCredentials: true, // Allows sending cookies and auth headers
 });
