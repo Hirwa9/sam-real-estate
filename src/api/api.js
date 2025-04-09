0bcaea708f4b9704b4ff5155d47516345f05f3a4
hirwa9 <hirwawilly9@gmail.com>
2025-04-09 11:51:36 +0200

    Add currency conversion API

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 export const BASE_URL = process.env.REACT_APP_BASE_URL;
+export const CURRENCY_KEY = process.env.REACT_APP_CURRENCY_API_KEY;
 
 export const Axios = axios.create({
     baseURL: BASE_URL,
